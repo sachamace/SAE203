79072bf94b795c13f6a3e5472829804ea11d3f8c
Sacha <sacha.mace@gmail.com>
2025-05-12 15:31:45 +0200

    pull sur site web

--- script.js
+++ script.js
@@ -19,39 +19,57 @@ async function fetchPokemon(id) {
     cry: `https://play.pokemonshowdown.com/audio/cries/${data.name.toLowerCase()}.mp3`
   };
 }
-
-async function loadPokedex() {
+function padId(id) {
+  return id.toString().padStart(3, '0');
+}
+function loadPokedex() {
   for (let i = 1; i <= 151; i++) {
-    const pokemon = await fetchPokemon(i);
+    const idPadded = padId(i);
+    
     const col = document.createElement('div');
     col.className = 'col';
+
     col.innerHTML = `
       <div class="card h-100">
-        <img src="https://raw.githubusercontent.com/ProjectPokemonSprites/sprites/master/anim/normal/${pokemon.spriteName}.gif" class="card-img-top animate__animated animate__pulse" alt="${pokemon.name}" style="cursor: pointer;">
+        <img 
+          src="/home/etuinfo/samace/COURS/Réseau/SAE203/SAE203/images_png_pokemon/${idPadded}.png"
+          data-gif="/home/etuinfo/samace/COURS/Réseau/SAE203/SAE203/images_gif_pokemon/${idPadded}.gif"
+          class="card-img-top"
+          alt="Pokemon #${idPadded}" 
+          style="cursor: pointer;">
         <div class="card-body">
-          <h5 class="card-title">${pokemon.name}</h5>
-          <p class="card-text">#${pokemon.id.toString().padStart(3, '0')}</p>
+          <h5 class="card-title">#${idPadded}</h5>
         </div>
       </div>
     `;
 
     const img = col.querySelector('img');
+     
     img.addEventListener('click', () => {
       const audio = new Audio(pokemon.cry);
       audio.play();
       img.classList.remove('animate__pulse');
       void img.offsetWidth; // reset animation
-      img.classList.add('animate__pulse');
+      img.classList.add('animate__pulse');x
+    });
+    img.addEventListener('mouseenter', () => {
+      img.src = img.getAttribute('data-gif');
+    });
+
+    img.addEventListener('mouseleave', () => {
+      img.src = `/home/etuinfo/samace/COURS/Réseau/SAE203/SAE203/images_png_pokemon/${idPadded}.png`;
     });
 
     pokedex.appendChild(col);
   }
+
+
+  // Ajouter Animate.css
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css';
+  document.head.appendChild(link);
 }
-
-// Ajouter Animate.css
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css';
-document.head.appendChild(link);
-
 loadPokedex();
+
+
